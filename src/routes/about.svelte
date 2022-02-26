1be3608cd5f79b4510058636538c7b37ae044c81
Jens E. Pedersen <jensegholm@protonmail.com>
2022-02-26 20:06:04 +0100

    Moved to flexbox and updated about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,67 +1,89 @@
+<script context="module">
+  export async function preload({ params, query }) {
+    return;
+  }
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <figure>
-	<img src="./jens.png" alt="" width="300" />
-		<figcaption>
-			Foto by <a href="https://www.linkedin.com/in/kevlee1/" title="Taken in Spain, 2019">Kevin Lee</a>.
-		</figcaption>
+  <img src="./jens.png" alt="" width="300" />
+  <figcaption>
+    Foto by <a
+      href="https://www.linkedin.com/in/kevlee1/"
+      title="Taken in Spain, 2019">Kevin Lee</a
+    >.
+  </figcaption>
 </figure>
 
-
-<section class='content'>
-	<h1>About</h1>
-	<p>Jens Egholm Pedersen is a doctoral student at the
-	<a href="https://www.kth.se/profile/jeped/" title="KTH">Royal Institute of Technology (KTH)</a>.
-	
-	He is currently working on spiking neural networks, dynamic vision systems, neuromorphic computing, and robots.
-	</p>
-	<p>
-	Find him on <a href="https://twitter.com/jensegholm" title="Twitter">Twitter</a> 
-	or
-	<a href="https://github.com/jegp/" title="GitHub">GitHub</a>.
-	</p>
+<section class="content">
+  <h1>About</h1>
+  <p>
+    Jens Egholm Pedersen is a doctoral student at the
+    <a href="https://www.kth.se/profile/jeped/" title="KTH"
+      >Royal Institute of Technology (KTH)</a
+    >. He is currently working with spiking neural networks, dynamic vision
+    systems, neuromorphic computing, and neurorobotics.
+  </p>
+  <p>
+    When faced with the complexity and ambiguity in the real world, contemporary
+    algorithms fail spectacularly. There is a <b
+      >strong need for robust, event-driven, and massively parallel algorithms
+      to solve our everyday physical problems</b
+    >.
+  </p>
+  <p>
+    By simulating and carefully scrutinizing and understanding neural circuits
+    that perform meaningful functions, including vision, motor control, and
+    self-sustenance, Jens seeks to <b
+      >build autonomous systems that perform meaningful work</b
+    >. Most robotic systems are staggeringly incompetent compared to even the
+    simplest members of the animal kingdom. I approach this bottom-up, tightly
+    following the Feynman axiom "What I cannot create, I do not understand".
+  </p>
+  <p>
+    Find Jens on <a href="https://twitter.com/jensegholm" title="Twitter"
+      >Twitter</a
+    >
+    or
+    <a href="https://github.com/jegp/" title="GitHub">GitHub</a>.
+  </p>
 </section>
 
-<script context="module">
-	export async function preload({ params, query }) {
-		return;
-	}
-</script>
-
 <style>
-.content {
-	max-width: 56em;
-	padding: 1em 2em 2em;
-	margin: 0 auto;
-	text-align: left;
-}
+  .content {
+    max-width: 56em;
+    padding: 1em 2em 2em;
+    margin: 0 auto;
+    text-align: center;
+  }
 
-figure {
-	margin: 0 auto;
-}
+  figure {
+    margin: 0 auto;
+  }
 
-figure img {
-	height: auto;
-	max-height: 512px;
-	max-width: 100%;
-}
+  figure img {
+    height: auto;
+    max-height: 512px;
+    max-width: 100%;
+  }
 
-figcaption {
-	display: block;
-	font-size: .875em;
-	font-style: italic;
-	line-height: 2.5em;
-	padding: 0 1em;
-}
+  figcaption {
+    display: block;
+    font-size: 0.875em;
+    font-style: italic;
+    line-height: 2.5em;
+    padding: 0 1em;
+  }
 
-h1 {
-	margin: 1em 0 .25em;
-}
+  h1 {
+    margin: 1em 0 0.25em;
+  }
 
-h1,
-p {
-	text-align: center;
-}
+  h1,
+  p {
+    text-align: center;
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,113 +1,126 @@
-<svelte:head>
-	<title>J. E. Pedersen</title>
-</svelte:head>
-
-<p>
-Research, thoughts and notes of Jens E. Pedersen.
-</p>
-<p>
-<iframe src="https://github.com/sponsors/Jegp/button" title="Sponsor Jegp" height="35" width="116" style="border: 0;"></iframe>
-&nbsp;</p>
-<br>
-<br>
-
-<h2>Posts</h2>
-<br/>
-<ul>
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
-		<li>
-			<a rel='prefetch' href='{post.slug}'>
-				{#if post.image}
-					<figure>
-						<span><img src="{post.image}" alt="" /></span>
-					</figure>
-				{/if}
-				{post.title}
-			</a>
-			<span class="date">{post.pubdate}</span>
-		</li>
-	{/each}
-</ul>
-
-<style>
-	figure img {
-		background-color: transparent;
-		max-height: 202px;
-		width: auto;
-	}
-
-	figure span {
-		align-items: flex-end;
-		display: -webkit-box;
-		display: -moz-box;
-		display: -ms-flexbox;
-		display: -webkit-flex;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-	}
-
-	h2 {
-		font-size: 1.8em;
-		text-transform: uppercase;
-		font-weight: 600;
-		margin: 0.25em 0 0.0625em;
-	}
-
-	ul, p {
-		align-items: flex-start;
-		display: -webkit-box;
-		display: -moz-box;
-		display: -ms-flexbox;
-		display: -webkit-flex;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: center;
-		line-height: 1.5;
-		list-style: none;
-		margin: 0 0 1em 0;
-		padding: 0;
-	}
-
-	ul li {
-		list-style-type: none;
-		padding: 11px;
-		text-align: center;
-	}
-	ul li:hover {
-		border-bottom: 1px solid black;
-		padding-bottom: 10px;
-	}
-
-	li a {
-		display: block;
-	}
-
-	.date {
-		color: rgba(0, 0, 0, 0.5);
-		font-size:90%;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 3em;
-		}
-	}
-</style>
-
-
 <script context="module">
-	export async function preload({ params, query }) {
-			return this.fetch(`index.json`).then(r => r.json()).then(posts => {
-				return { posts };
-			});
-		}
+  export async function preload({ params, query }) {
+    return this.fetch(`index.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        return { posts };
+      });
+  }
 </script>
 
 <script>
-	export let posts;
+  export let posts;
 </script>
+
+<svelte:head>
+  <title>J. E. Pedersen</title>
+</svelte:head>
+
+<div class="container">
+  <p>Research, thoughts, and notes of Jens E. Pedersen.</p>
+  <p>
+    <iframe
+      src="https://github.com/sponsors/Jegp/button"
+      title="Sponsor Jegp"
+      height="35"
+      width="116"
+      style="border: 0;"
+    />
+    &nbsp;
+  </p>
+  <br />
+  <br />
+
+  <h1>Posts</h1>
+  <br />
+  <div class="flex-container">
+    {#each posts as post}
+      <!-- we're using the non-standard `rel=prefetch` attribute to
+				tell Sapper to load the data for the page as soon as
+				the user hovers over the link or taps it, instead of
+				waiting for the 'click' event -->
+      <div>
+        <a rel="prefetch" href={post.slug}>
+          {#if post.image}
+            <figure>
+              <span><img src={post.image} alt="" /></span>
+            </figure>
+          {/if}
+          <h3>{post.title}</h3>
+          <span class="date">{post.pubdate}</span>
+        </a>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container {
+    text-align: center;
+  }
+
+  figure img {
+    background-color: transparent;
+    max-height: 202px;
+    width: auto;
+  }
+
+  figure span {
+    align-items: flex-end;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 2.5em;
+    text-transform: uppercase;
+    font-weight: 600;
+    margin: 0.25em 0 0.0625em;
+  }
+
+  .flex-container {
+    align-content: space-around;
+    align-items: stretch;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px 20px;
+    justify-content: center;
+  }
+
+  .flex-container div {
+    border: 2px solid #f1f1f1;
+    border-radius: 1rem;
+    box-shadow: 3px 2px 6px #f1f1f1;
+    display: flex;
+    padding: 1em;
+    position: relative;
+	margin-left: auto;
+	margin-right: auto;
+    text-align: center;
+  }
+
+  .flex-container div a {
+    display: inline-block;
+    align-self: flex-end;
+	text-decoration: none;
+  }
+  .flex-container div a h3 {
+	text-decoration: underline;
+  }
+
+  .date {
+    color: rgba(0, 0, 0, 0.5);
+    font-size: 90%;
+  }
+
+  @media (max-width: 600px) {
+    .flex-container {
+      flex-direction: column;
+    }
+  }
+</style>
