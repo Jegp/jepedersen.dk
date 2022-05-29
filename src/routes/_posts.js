7c865e06d18b8c2478a5ea77273f0e2d8fdc423f
Jens Egholm <jensegholm@protonmail.com>
2022-05-29 21:30:32 +0200

    Added lambda post

--- src/routes/_posts.js
+++ src/routes/_posts.js
@@ -29,52 +29,53 @@ export function getPost(slug) {
   // Replace with link to HTML
   if (metadata.link) {
     slug = metadata.link;
-  }
-  const html_markdown = marked(content);
-  var html = process_latex(html_markdown);
+  } else {
+    const html_markdown = marked(content);
+    var html = process_latex(html_markdown);
 
-  // Include bibliography and sort out citations
-  if (metadata.bibliography) {
-    const reference_regex = /\[@(?<key>\w*)\]/gu;
-    const bib = fs.readFileSync(`static/${metadata.bibliography}`, "utf-8");
-    const dict = process_bibtex(bib);
-    const ref_list = [];
-    // Extract references
-    var m = "";
-    while ((m = reference_regex.exec(html))) {
-      const ref_key = m.groups.key;
-      if (ref_list.indexOf(ref_key) == -1) {
-        ref_list.push(ref_key);
+    // Include bibliography and sort out citations
+    if (metadata.bibliography) {
+      const reference_regex = /\[@(?<key>\w*)\]/gu;
+      const bib = fs.readFileSync(`static/${metadata.bibliography}`, "utf-8");
+      const dict = process_bibtex(bib);
+      const ref_list = [];
+      // Extract references
+      var m = "";
+      while ((m = reference_regex.exec(html))) {
+        const ref_key = m.groups.key;
+        if (ref_list.indexOf(ref_key) == -1) {
+          ref_list.push(ref_key);
+        }
+        const index = ref_list.length - 1;
+        html = html.replace(
+          new RegExp(`@${ref_key}`, 'gu'),
+          `<a href="${slug}/#${index}">${index + 1}</a>`
+        );
       }
-      const index = ref_list.length - 1;
-      html = html.replace(
-        new RegExp(`@${ref_key}`, 'gu'),
-        `<a href="${slug}/#${index}">${index + 1}</a>`
-      );
-    }
-    // Build bibliography
-    html += "<h2>References</h2><ol class='references'>";
-    for (const key in ref_list) {
-      const entry = dict[ref_list[key]];
-      const journal =
-        entry["journal"] ||
-        entry["booktitle"] ||
-        entry["publisher"] ||
-        entry["url"];
-      // Entry HTML
-      var entry_html = `${entry["author"]}: <i>${entry["title"]}</i>, ${entry["year"]}, ${journal}`;
-      // Entry URL (if present)
-      if ("url" in entry) {
-        entry_html = `<a href="${entry["url"]}">${entry_html}</a>`;
-      } else if ("doi" in entry) {
-        entry_html = `<a href="https://doi.org/${entry["doi"]}">${entry_html}</a>`;
-      }
+      // Build bibliography
+      html += "<h2>References</h2><ol class='references'>";
+      for (const key in ref_list) {
+        const entry = dict[ref_list[key]];
+        const journal =
+          entry["journal"] ||
+          entry["booktitle"] ||
+          entry["publisher"] ||
+          entry["url"];
+        // Entry HTML
+        var entry_html = `${entry["author"]}: <i>${entry["title"]}</i>, ${entry["year"]}, ${journal}`;
+        // Entry URL (if present)
+        if ("url" in entry) {
+          entry_html = `<a href="${entry["url"]}">${entry_html}</a>`;
+        } else if ("doi" in entry) {
+          entry_html = `<a href="https://doi.org/${entry["doi"]}">${entry_html}</a>`;
+        }
 
-      // Entry li
-      entry_html = `<li id="${key}">${entry_html}</li>`;
-      html += entry_html;
+        // Entry li
+        entry_html = `<li id="${key}">${entry_html}</li>`;
+        html += entry_html;
+      }
+      html += "</ol>";
     }
-    html += "</ol>";
   }
 
   return {
--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware({ ignore: ['/static/slides/*']})
+		sapper.middleware({ ignore: ['/static/slides/*', '/static/pdf/*']})
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
