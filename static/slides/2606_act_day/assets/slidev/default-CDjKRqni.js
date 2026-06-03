d460bb0676cca69ea5a35a4be04bcc0fb213e353
jegp <jens@jepedersen.dk>
2026-06-03 09:35:36 +0200

    Fixed path to act slides

---
+++ static/slides/2606_act_day/assets/slidev/default-CDjKRqni.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-DaU6Xtj-.js";import{_ as r}from"../index-B4-7jQdP.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
