08e95fadd3f8131974a808dd2bcec50a923492e3
jegp <jens@jepedersen.dk>
2026-06-30 11:14:33 +0200

    Added SDU slides

---
+++ static/slides/2606_sdu/assets/slidev/default-7zOFWIMZ.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-D1rKwxQ9.js";import{_ as r}from"../index-CN0v6trY.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
